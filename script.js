7db337e4b3d0f9425e11435a2da69b5ed2ceba7b
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-24 15:32:35 +0200

    add structure

---
+++ script.js
@@ -0,0 +1,69 @@
+new Vue({
+    el: "#app",
+
+    data: {
+         
+        headbarLinks:[
+            {
+              text: "Shopping Cart",
+              link: "#"
+            },
+
+            {
+                text:"My Account",
+                link: "#"
+            },
+
+            {
+                text:"Cart",
+                link:"#",
+                icon: "fa-shopping-cart"
+            }
+        ],
+
+        navbarLinks: [
+            {
+                text: "Home",
+                link: "#",
+            },
+
+            {
+                text: "Shop",
+                link: "#",
+            },
+
+            {
+                text: "Products",
+                link: "#",
+            },
+
+            {
+                text: "Categories",
+                link: "#",
+            },
+
+            {
+                text: "News",
+                link: "#",
+            },
+
+            {
+                text: "Elements",
+                link: "#",
+            },
+
+            {
+                text: "SHOP NOW!",
+                link: "#",
+            },
+
+            {
+                text: "",
+                link: "",
+                icon:"fa-search"
+            },
+
+
+        ]
+    }
+})
